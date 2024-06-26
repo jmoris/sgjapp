a5531530a16db604202343e0c72135d18ad74031
Jesus Moris <jesus@soluciontotal.cl>
2024-06-26 00:06:11 -0400

    condicion sidebar ventas

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -16,148 +16,153 @@
                     <span class="link-title">Dashboard</span>
                 </a>
             </li>
-            @if(has_permission('ver-administracion')||has_permission('ver-usuario'))
-            <li class="nav-item nav-category">ADMINISTRACIÓN</li>
+            @if (has_permission('ver-administracion') || has_permission('ver-usuario'))
+                <li class="nav-item nav-category">ADMINISTRACIÓN</li>
             @endif
-            @if(has_permission('ver-usuario'))
-            <li class="nav-item {{ active_class(['usuarios*']) }}">
-                <a href="{{ url('/usuarios') }}" class="nav-link">
-                    <i class="mdi mdi-folder-account mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Usuarios</span>
-                </a>
-            </li>
+            @if (has_permission('ver-usuario'))
+                <li class="nav-item {{ active_class(['usuarios*']) }}">
+                    <a href="{{ url('/usuarios') }}" class="nav-link">
+                        <i class="mdi mdi-folder-account mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Usuarios</span>
+                    </a>
+                </li>
             @endif
-            @if(has_permission('ver-administracion'))
-            <li class="nav-item {{ active_class(['permisos*']) }}">
-                <a href="{{ url('/permisos') }}" class="nav-link">
-                    <i class="mdi mdi-shield-account mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Roles y Permisos</span>
-                </a>
-            </li>
-            <li class="nav-item {{ active_class(['ajustes*']) }}">
-                <a href="{{ url('/ajustes') }}" class="nav-link">
-                    <i class="mdi mdi-cog mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Ajustes Generales</span>
-                </a>
-            </li>
+            @if (has_permission('ver-administracion'))
+                <li class="nav-item {{ active_class(['permisos*']) }}">
+                    <a href="{{ url('/permisos') }}" class="nav-link">
+                        <i class="mdi mdi-shield-account mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Roles y Permisos</span>
+                    </a>
+                </li>
+                <li class="nav-item {{ active_class(['ajustes*']) }}">
+                    <a href="{{ url('/ajustes') }}" class="nav-link">
+                        <i class="mdi mdi-cog mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Ajustes Generales</span>
+                    </a>
+                </li>
             @endif
             <li class="nav-item nav-category">MAESTROS</li>
-            @if(has_permission('ver-proveedor'))
-            <li class="nav-item {{ active_class(['proveedores*']) }}">
-                <a href="{{ url('/proveedores') }}" class="nav-link">
-                    <i class="mdi mdi-truck-delivery-outline mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Proveedores</span>
-                </a>
-            </li>
+            @if (has_permission('ver-proveedor'))
+                <li class="nav-item {{ active_class(['proveedores*']) }}">
+                    <a href="{{ url('/proveedores') }}" class="nav-link">
+                        <i class="mdi mdi-truck-delivery-outline mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Proveedores</span>
+                    </a>
+                </li>
             @endif
-            @if(has_permission('ver-cliente'))
-            <li class="nav-item {{ active_class(['clientes*']) }}">
-                <a href="{{ url('/clientes') }}" class="nav-link">
-                    <i class="mdi mdi-account-group mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Clientes</span>
-                </a>
-            </li>
+            @if (has_permission('ver-cliente'))
+                <li class="nav-item {{ active_class(['clientes*']) }}">
+                    <a href="{{ url('/clientes') }}" class="nav-link">
+                        <i class="mdi mdi-account-group mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Clientes</span>
+                    </a>
+                </li>
             @endif
-            @if(has_permission('ver-producto'))
-            <li class="nav-item {{ active_class(['productos*']) }}">
-                <a href="{{ url('/productos') }}" class="nav-link">
-                    <i class="mdi mdi-package-variant mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Productos</span>
-                </a>
-            </li>
+            @if (has_permission('ver-producto'))
+                <li class="nav-item {{ active_class(['productos*']) }}">
+                    <a href="{{ url('/productos') }}" class="nav-link">
+                        <i class="mdi mdi-package-variant mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Productos</span>
+                    </a>
+                </li>
             @endif
-            @if(has_permission('ver-bodega'))
-            <li class="nav-item {{ active_class(['bodegas']) }}">
-                <a href="#" class="nav-link">
-                    <i class="mdi mdi-warehouse mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Bodegas</span>
-                </a>
-            </li>
+            @if (has_permission('ver-bodega'))
+                <li class="nav-item {{ active_class(['bodegas']) }}">
+                    <a href="#" class="nav-link">
+                        <i class="mdi mdi-warehouse mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Bodegas</span>
+                    </a>
+                </li>
             @endif
-            @if(has_permission('ver-centro-costo'))
-            <li class="nav-item {{ active_class(['centrocosto']) }}">
-                <a href="#" class="nav-link">
-                    <i class="mdi mdi-finance mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Centros de Costos</span>
-                </a>
-            </li>
+            @if (has_permission('ver-centro-costo'))
+                <li class="nav-item {{ active_class(['centrocosto']) }}">
+                    <a href="#" class="nav-link">
+                        <i class="mdi mdi-finance mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Centros de Costos</span>
+                    </a>
+                </li>
             @endif
             <li class="nav-item nav-category">VENTAS</li>
-            @if(has_permission('ver-proyecto'))
-            <li class="nav-item {{ active_class(['ventas/proyectos*']) }}">
-                <a href="{{ url('ventas/proyectos') }}" class="nav-link">
-                    <i class="mdi mdi-file-document-arrow-right mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Proyectos</span>
-                </a>
-            </li>
+            @if (has_permission('ver-proyecto'))
+                <li class="nav-item {{ active_class(['ventas/proyectos*']) }}">
+                    <a href="{{ url('ventas/proyectos') }}" class="nav-link">
+                        <i class="mdi mdi-file-document-arrow-right mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Proyectos</span>
+                    </a>
+                </li>
             @endif
             <li class="nav-item {{ active_class(['ventas/*']) }}">
-                <a class="nav-link" data-bs-toggle="collapse" href="#email" role="button" aria-expanded="{{ is_active_route(['email/*']) }}" aria-controls="email">
+                <a class="nav-link" data-bs-toggle="collapse" href="#email" role="button"
+                    aria-expanded="{{ is_active_route(['email/*']) }}" aria-controls="email">
                     <i class="mdi mdi-file-document-multiple-outline mdi-18"></i>
-                  <span class="link-title" style="margin-left: 12px;">Documentos</span>
-                  <i class="link-arrow" data-feather="chevron-down"></i>
+                    <span class="link-title" style="margin-left: 12px;">Documentos</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
                 </a>
+                @if (has_permission('ver-factura'))
                 <div class="collapse {{ show_class(['ventas/*']) }}" id="email">
-                  <ul class="nav sub-menu">
-                    <li class="nav-item">
-                        <a href="{{ url('/ventas/facturas') }}" class="nav-link {{ active_class(['ventas/facturas']) }}">
-                            Facturas
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ url('/ventas/facturas') }}" class="nav-link {{ active_class(['email/guiasdespacho']) }}">
-                            Guias de Despacho
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ url('/ventas/facturas') }}" class="nav-link {{ active_class(['email/notascredito']) }}">
-                            Notas de Credito
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ url('/ventas/facturas') }}" class="nav-link {{ active_class(['email/notasdebito']) }}">
-                            Notas de Debito
-                        </a>
-                    </li>
-                  </ul>
+                    <ul class="nav sub-menu">
+                        @if (has_permission('ver-factura'))
+                            <li class="nav-item">
+                                <a href="{{ url('/ventas/facturas') }}"
+                                    class="nav-link {{ active_class(['ventas/facturas']) }}">
+                                    Facturas
+                                </a>
+                            </li>
+                        @endif
+                        <li class="nav-item">
+                            <a href="{{ url('/ventas/facturas') }}"
+                                class="nav-link {{ active_class(['email/guiasdespacho']) }}">
+                                Guias de Despacho
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ url('/ventas/facturas') }}"
+                                class="nav-link {{ active_class(['email/notascredito']) }}">
+                                Notas de Credito
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ url('/ventas/facturas') }}"
+                                class="nav-link {{ active_class(['email/notasdebito']) }}">
+                                Notas de Debito
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-              </li>
-
-            @if(has_permission('ver-factura'))
-
-            @endif
-            @if(has_permission('ver-presupuesto'))
-            <li class="nav-item {{ active_class(['apps/presupuestos']) }}">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="message-square"></i>
-                    <span class="link-title">Presupuestos</span>
-                </a>
             </li>
             @endif
-            @if(has_permission('ver-pago'))
-            <li class="nav-item {{ active_class(['apps/pagos']) }}">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="message-square"></i>
-                    <span class="link-title">Pagos</span>
-                </a>
-            </li>
+            @if (has_permission('ver-presupuesto'))
+                <li class="nav-item {{ active_class(['apps/presupuestos']) }}">
+                    <a href="#" class="nav-link">
+                        <i class="link-icon" data-feather="message-square"></i>
+                        <span class="link-title">Presupuestos</span>
+                    </a>
+                </li>
+            @endif
+            @if (has_permission('ver-pago'))
+                <li class="nav-item {{ active_class(['apps/pagos']) }}">
+                    <a href="#" class="nav-link">
+                        <i class="link-icon" data-feather="message-square"></i>
+                        <span class="link-title">Pagos</span>
+                    </a>
+                </li>
             @endif
             <li class="nav-item nav-category">COMPRAS</li>
-            @if(has_permission('ver-orden-compra'))
-            <li class="nav-item {{ active_class(['compras/ordenescompra*']) }}">
-                <a href="{{ url('compras/ordenescompra') }}" class="nav-link">
-                    <i class="mdi mdi-file-document-arrow-right-outline mdi-18"></i>
-                    <span class="link-title" style="margin-left: 12px;">Ordenes de Compra</span>
-                </a>
-            </li>
+            @if (has_permission('ver-orden-compra'))
+                <li class="nav-item {{ active_class(['compras/ordenescompra*']) }}">
+                    <a href="{{ url('compras/ordenescompra') }}" class="nav-link">
+                        <i class="mdi mdi-file-document-arrow-right-outline mdi-18"></i>
+                        <span class="link-title" style="margin-left: 12px;">Ordenes de Compra</span>
+                    </a>
+                </li>
             @endif
-            @if(has_permission('ver-inventario'))
-            <li class="nav-item {{ active_class(['apps/inventario']) }}">
-                <a href="#" class="nav-link">
-                    <i class="link-icon" data-feather="message-square"></i>
-                    <span class="link-title">Inventario</span>
-                </a>
-            </li>
+            @if (has_permission('ver-inventario'))
+                <li class="nav-item {{ active_class(['apps/inventario']) }}">
+                    <a href="#" class="nav-link">
+                        <i class="link-icon" data-feather="message-square"></i>
+                        <span class="link-title">Inventario</span>
+                    </a>
+                </li>
             @endif
         </ul>
     </div>
