7b7f5f3de05312c85672bed4624a3a43b563cc98
Jesus Moris <jesus@soluciontotal.cl>
2024-11-14 01:52:28 -0300

    fixy

--- app/Http/Controllers/FacturaCompraController.php
+++ app/Http/Controllers/FacturaCompraController.php
@@ -30,7 +30,7 @@ class FacturaCompraController extends Controller
             curl_close($ch);
             $docData = json_decode($result);
             Log::info("ENDPOINT FACTURAS COMPRA: ". $endpoint);
-            Log::info($docData);
+            Log::info(print_r($docData, true));
             if($docData['success'] == true){
                 return DataTables::of($docData)->toJson();
             }
