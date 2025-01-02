e3d8ae9e43a1d718b6541ce0dde90baee8753694
Jesus Moris <jesus@soluciontotal.cl>
2025-01-02 17:19:52 -0300

    icono boton lista facturas compra

--- resources/views/pages/compras/facturas/index.blade.php
+++ resources/views/pages/compras/facturas/index.blade.php
@@ -38,7 +38,7 @@
                                                 <td>{{ $doc->rut_emisor }}</td>
                                                 <td>{{ $doc->fecha_emision }}</td>
                                                 <td>$ {{ number_format($doc->monto_total, 0, ',', '.') }}</td>
-                                                <td><button type="button" title="Ver Factura de Compra" onclick="verDocumento('{{$doc->rut_emisor}}', {{$doc->folio}})" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-document"></i></button><button type="button" title="Ver Vista Previa Factura de Compra" onclick="vistaPreviaDocumento('{{$doc->rut_emisor}}', {{$doc->tipo_doc}}, {{$doc->folio}})" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-magnify"></i></button></td>
+                                                <td><button type="button" title="Ver Factura de Compra" onclick="verDocumento('{{$doc->rut_emisor}}', {{$doc->folio}})" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-text-box-search-outline"></i></button><button type="button" title="Ver Vista Previa Factura de Compra" onclick="vistaPreviaDocumento('{{$doc->rut_emisor}}', {{$doc->tipo_doc}}, {{$doc->folio}})" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-magnify"></i></button></td>
                                             </tr>
                                             @endforeach
                                         </tbody>
@@ -66,7 +66,7 @@
 
 
         function verDocumento(emisor, folio) {
-            window.open(`/compras/facturas/detalle/${emisor}/${folio}`);
+            location.href = `/compras/facturas/detalle/${emisor}/${folio}`;
         }
 
         function vistaPreviaDocumento(emisor, tipo, folio) {
