4f4f6916cc68896129915417db085b11d8992f81
Jesus Moris <jesus@soluciontotal.cl>
2025-01-03 03:52:58 -0300

    fix dd

--- resources/views/pages/compras/facturas/detail.blade.php
+++ resources/views/pages/compras/facturas/detail.blade.php
@@ -253,14 +253,9 @@
                                                         if (!isset($referencias[0])) {
                                                             $referencias = [$referencias];
                                                         }
+                                                        dd($referencias);
                                                     @endphp
-                                                    @foreach ($referencias as $ref)
-                                                        <tr>
-                                                            <td>{{ $ref['TpoDocRef'] }}</td>
-                                                            <td>{{ $ref['FolioRef'] }}</td>
-                                                            <td>{{ $ref['FchRef'] }}</td>
-                                                        </tr>
-                                                    @endforeach
+
 
                                                     </tbody>
                                                 </table>
