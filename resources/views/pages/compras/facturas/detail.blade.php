2945b1a31244fcac5eb3386b3393fffeed62cd02
Jesus Moris <jesus@soluciontotal.cl>
2025-01-03 03:00:43 -0300

    alineacion vertical

--- resources/views/pages/compras/facturas/detail.blade.php
+++ resources/views/pages/compras/facturas/detail.blade.php
@@ -290,7 +290,7 @@
                     </div>
                 </div>
                 <div class="col-md-4 grid-margin stretch-card">
-                    <div class="row">
+                    <div class="row align-items-start">
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-body">
