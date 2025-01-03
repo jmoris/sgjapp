21d62b13cc937976ab09b5f94b8b3bf324531945
Jesus Moris <jesus@soluciontotal.cl>
2025-01-03 02:10:47 -0300

    se cuadra div

--- resources/views/pages/compras/facturas/detail.blade.php
+++ resources/views/pages/compras/facturas/detail.blade.php
@@ -374,36 +374,15 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="d-flex justify-content-between align-items-baseline">
-                                <h4 class="card-title mb-0">VISOR DE FACTURAS DE COMPRA ELECTRONICA</h4>
+                                <h4 class="card-title mb-0">PAGOS ASOCIADOS AL DOCUMENTO</h4>
                             </div>
-                            <div class="row mx-3">
-                                <div style="width:100%; margin-top:24px;"></div>
-                                <div class="col-md-12">
-                                    <div class="row">
-                                        <div class="col-md-12 mb-3">
-                                            <div class="mb-2 border-bottom">
-                                                <h5>Pagos asociados</h5>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <table class="table">
-                                                        <thead>
-                                                            <th>Tipo</th>
-                                                            <th>Fecha</th>
-                                                            <th>Monto</th>
-                                                        </thead>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <button type="button" class="btn btn-danger"
-                                onclick="location.href = '/compras/facturas'">
-                                <i class="mdi mdi-cancel"></i>
-                                Cerrar
-                            </button>
+                            <table class="table">
+                                <thead>
+                                    <th>Tipo</th>
+                                    <th>Fecha</th>
+                                    <th>Monto</th>
+                                </thead>
+                            </table>
                         </div>
                     </div>
                 </div>
