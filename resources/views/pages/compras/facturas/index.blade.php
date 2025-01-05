f1759d883a64f612c71e3756b8f88c06df83baf0
Jesus Moris <jesus@soluciontotal.cl>
2025-01-05 03:02:58 -0300

    rango fecha en guias compra

--- resources/views/pages/compras/facturas/index.blade.php
+++ resources/views/pages/compras/facturas/index.blade.php
@@ -19,7 +19,7 @@
                             </div>
                             <div class="row">
                                 <div class="col-12">
-                                    <div class="col-12">
+                                    <div class="col-12 mx-2">
                                         Desde: <input type="text" id="min" name="min">
                                         Hasta:  <input type="text" id="max" name="max">
                                     </div>
@@ -40,7 +40,7 @@
                                                 <td>{{ $doc->folio }}</td>
                                                 <td>{{ $doc->razonsocial_emisor }}</td>
                                                 <td>{{ $doc->rut_emisor }}</td>
-                                                <td>{{ $doc->fecha_emision }}</td>
+                                                <td>{{ date('d/m/Y', strtotime($doc->fecha_emision)) }}</td>
                                                 <td>$ {{ number_format($doc->monto_total, 0, ',', '.') }}</td>
                                                 <td><div><button type="button" title="Ver Factura de Compra" onclick="verDocumento('{{$doc->rut_emisor}}', {{$doc->folio}})" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-text-box-search-outline"></i></button><button type="button" title="Ver Vista Previa Factura de Compra" onclick="vistaPreviaDocumento('{{$doc->rut_emisor}}', {{$doc->tipo_doc}}, {{$doc->folio}})" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-magnify"></i></button></div></td>
                                             </tr>
--- resources/views/pages/compras/guias/index.blade.php
+++ resources/views/pages/compras/guias/index.blade.php
@@ -19,6 +19,10 @@
                             </div>
                             <div class="row">
                                 <div class="col-12">
+                                    <div class="col-12 mx-2">
+                                        Desde: <input type="text" id="min" name="min">
+                                        Hasta:  <input type="text" id="max" name="max">
+                                    </div>
                                     <table id="example" class="compact hover order-column row-border" style="width:100%">
                                         <thead>
                                             <tr>
@@ -36,7 +40,7 @@
                                                 <td>{{ $doc->folio }}</td>
                                                 <td>{{ $doc->razonsocial_emisor }}</td>
                                                 <td>{{ $doc->rut_emisor }}</td>
-                                                <td>{{ $doc->fecha_emision }}</td>
+                                                <td>{{ date('d/m/Y', strtotime($doc->fecha_emision)) }}</td>
                                                 <td>$ {{ number_format($doc->monto_total, 0, ',', '.') }}</td>
                                                 <td><div><button type="button" title="Ver Factura de Compra" onclick="verDocumento('{{$doc->rut_emisor}}', {{$doc->folio}})" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-text-box-search-outline"></i></button><button type="button" title="Ver Factura de Compra" onclick="vistaPreviaDocumento('{{$doc->rut_emisor}}', {{$doc->tipo_doc}}, {{$doc->folio}})" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-magnify"></i></button></div></td>
                                             </tr>
@@ -84,5 +88,36 @@
                 [3, 'desc']
             ]
         });
+
+ // Custom filtering function which will search data in column four between two values
+ DataTable.ext.search.push(function (settings, data, dataIndex) {
+     let min = minDate.val();
+     let max = maxDate.val();
+     let date = new Date(data[3]);
+
+     if (
+         (min === null && max === null) ||
+         (min === null && date <= max) ||
+         (min <= date && max === null) ||
+         (min <= date && date <= max)
+     ) {
+         return true;
+     }
+     return false;
+ });
+
+ // Create date inputs
+ minDate = new DateTime('#min', {
+     format: 'DD/MM/YYYY'
+ });
+ maxDate = new DateTime('#max', {
+     format: 'DD/MM/YYYY'
+ });
+
+
+ // Refilter the table
+ document.querySelectorAll('#min, #max').forEach((el) => {
+     el.addEventListener('change', () => facturasTable.draw());
+ });
     </script>
 @endpush
