029801724d196b46f6ca19017d81cf15b3256ed3
Jesus Moris <jesus@soluciontotal.cl>
2025-01-28 20:28:26 -0300

    cambios visor

--- resources/views/pages/compras/facturas/index.blade.php
+++ resources/views/pages/compras/facturas/index.blade.php
@@ -136,9 +136,9 @@
                             row.rut_emisor + '\',' + row.folio +
                             ')" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-text-box-search-outline"></i></button>';
 
-                            html += '<button type="button" title="Ver Vista Previa Factura de Compra" onclick="vistaPreviaDocumento(\'' +
-                            row.rut_emisor + '\',33,' + row.folio +
-                            ')" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-magnify"></i></button>';
+                            //html += '<button type="button" title="Ver Vista Previa Factura de Compra" onclick="vistaPreviaDocumento(\'' +
+                            //row.rut_emisor + '\',33,' + row.folio +
+                            //')" class="btn btn-outline-primary btnxs px-1 py-0 ms-1"><i class="mdi mdi-18 mdi-magnify"></i></button>';
                         }
                         html += '</div>';
                         //var html = '';
