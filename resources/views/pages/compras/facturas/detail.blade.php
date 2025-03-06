7f7eba386f73df4b21cf8abeb865f502e0585bd5
Jesus Moris <jesus@soluciontotal.cl>
2025-03-06 03:28:44 -0300

    fix error categorizacion

--- resources/views/pages/compras/facturas/detail.blade.php
+++ resources/views/pages/compras/facturas/detail.blade.php
@@ -507,7 +507,7 @@
 
             $.ajax({
                 type: "POST",
-                url: "/api/ventas/facturas/categorizar/{{intval($documento['Encabezado']['IdDoc']['Folio'])}}",
+                url: "/api/ventas/categorizar/factura/{{intval($documento['Encabezado']['IdDoc']['Folio'])}}",
                 data: data, // serializes the form's elements.
                 success: function(data){
                     $.toast({
