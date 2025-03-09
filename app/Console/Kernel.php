657dbc60571e2d315f4ab542da32d9f6e2aa12d6
Jesus Moris <jesus@soluciontotal.cl>
2025-03-09 00:42:24 -0300

    envio notificaciones al queue notificaciones

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -215,7 +215,7 @@ class Kernel extends ConsoleKernel
                         $doc->tiene_xml = true;
                         $doc->save();
                         try{
-                            Notification::send($users, new DocumentoRecibido($data->rut_emisor, 33, $data->folio));
+                            Notification::send($users, (new DocumentoRecibido($data->rut_emisor, 33, $data->folio))->onQueue('notificaciones'));
                         }catch(Exception $ex){
                             Log::error('Hubo un error al intentar enviar la notificacion del contriuyente '.$data->rut_emisor. ' folio '.$data->folio);
                         }
