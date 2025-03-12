0d41801746a8d3e11e707a6b126ce54e51056364
Jesus Moris <jesus@soluciontotal.cl>
2025-03-12 01:45:27 -0300

    mejora ui pagos

--- resources/views/pages/compras/facturas/detail.blade.php
+++ resources/views/pages/compras/facturas/detail.blade.php
@@ -499,7 +499,7 @@
                                                 <tr>
                                                     <td>{{$pago->tipo_pago}}</td>
                                                     <td>{{$pago->fecha_pago}}</td>
-                                                    <td>{{$pago->monto_pago}}</td>
+                                                    <td>$ {{ number_format($pago->monto_pago, 0, ',', '.') }}</td>
                                                     <td><button class="btn btn-sm btn-outline-danger" style="padding:.25em .5em; float:left;" onclick="eliminarPago({{ $pago->id }})"><i class="mdi mdi-delete"></i></button></td>
                                                 </tr>
                                                 @endforeach
