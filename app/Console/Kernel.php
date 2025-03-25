4bf42b38e50350cba659a3ff2da5f424913ac89d
Jesus Moris <jesus@soluciontotal.cl>
2025-03-24 21:01:28 -0300

    parte string

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -124,7 +124,7 @@ class Kernel extends ConsoleKernel
                         $result = curl_exec($ch);
                         curl_close($ch);
                         $docData = json_decode($result, true);
-                        $factEstado = substr_replace($doc->estado, $docData['email_recibido'], 1, 1);
+                        $factEstado = substr_replace(strval($doc->estado), strval($docData['email_recibido']), 1, 1);
                         Factura::where('id', $doc->id)->update(['estado' => $factEstado]);
                         Log::info("Factura ".$doc->folio." estado:".$doc->estado." nuevo estado:".$factEstado);
                     }
