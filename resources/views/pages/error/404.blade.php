cf4da829fd0666ebc6d75d83ef2948c993ced945
Jesus Moris <jesus@soluciontotal.cl>
2025-05-11 23:43:07 -0400

    Updates error 404 page heading
    Updates the 404 error page heading to include "ERROR" for clarity.

--- resources/views/pages/error/404.blade.php
+++ resources/views/pages/error/404.blade.php
@@ -6,7 +6,7 @@
   <div class="row w-100 mx-0 auth-page">
     <div class="col-md-8 col-xl-6 mx-auto d-flex flex-column align-items-center">
       <img src="{{ url('assets/images/others/404.svg') }}" class="img-fluid mb-2" alt="404">
-      <h1 class="fw-bolder mb-22 mt-2 tx-80 text-muted">404</h1>
+      <h1 class="fw-bolder mb-22 mt-2 tx-80 text-muted">ERROR 404</h1>
       <h4 class="mb-2">Página No Encontrada</h4>
       <h6 class="text-muted mb-3 text-center">Oopps!! La página que estas buscando al parecer no existe.</h6>
       <a href="{{ url('/') }}"><- Volver atras</a>
